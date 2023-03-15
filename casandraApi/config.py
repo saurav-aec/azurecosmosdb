cac3655eb8cc483a29f69dad753a7de8b959cb63
Saurav Banerjee <banersau@lab.morganstanley.com>
2023-03-15 20:18:33 +0000

    Added config.py

---
+++ casandraApi/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''Configure credentials from the environment'''
+import os
+
+print("Configuring credentials from the environment...")
+
+config = {
+    'port': '10350',
+    'username': 'azure-lab-leading-eagle-1873',
+    'password': 'Od8AokXcpmfGgn67NvCbAbM5JaSC7ErUWVryfrt5uJ1t1Mpkb2xpFGXK89zKGTyNZj8Jbwxllx41ACDbnbAZaA==',
+    'contactPoint': 'azure-lab-leading-eagle-1873.cassandra.cosmos.azure.com'
+}
+
+print("Configuration complete!")
